b7c1d6788a632bfdd3a8a0808453705a7d808dd3
Arrya-A <arrya98@gmail.com>
2024-11-20 11:14:07 +0530

    v3

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -5,42 +5,52 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ApiService {
+
   server_url = "http://localhost:3000"
-  constructor(private http: HttpClient) { }
 
-  getAllRecipeAPI() {
-    return this.http.get(`${this.server_url}/all-recipes`)
+  constructor(private http:HttpClient) { }
+
+  getAllRecipeAPI(){
+    return  this.http.get(`${this.server_url}/all-recipes`)
   }
-
-  //add-testimony
-  saveTestimonyAPI(reqBody: any) {
-    return this.http.post(`${this.server_url}/add-testimony`, reqBody)
+//add-testimony
+  saveTestimonyAPI(reqBody:any){
+    return  this.http.post(`${this.server_url}/add-testimony`,reqBody)
   }
 
   //register
-  registerAPI(reqBody: any) {
-    return this.http.post(`${this.server_url}/register`, reqBody)
+  registerAPI(reqBody:any){
+    return  this.http.post(`${this.server_url}/register`,reqBody)
   }
 
   //login
-  loginAPI(reqBody: any) {
-    return this.http.post(`${this.server_url}/login`, reqBody)
+  loginAPI(reqBody:any){
+    return  this.http.post(`${this.server_url}/login`,reqBody)
   }
 
-  
-//appendToken in header
-appendToken(){
-  const token = sessionStorage.getItem("token")
-  let headers = new HttpHeaders()
-  if(token){
-    headers=headers.append("Authorization", `Bearer ${token}`)
+  //append Token in header
+  appendToken(){
+    const token = sessionStorage.getItem("token")
+    let headers = new HttpHeaders()
+    if(token){
+     headers = headers.append("Authorization",`Bearer ${token}`)
+    }
+    return {headers}    
   }
-  return {headers}
-}
 
-//recipe/:id/view
-viewRecipeAPI(id:string){
-  return this.http.get(`${this.server_url}/recipe/${id}/view`,this.appendToken())
-}
-}
+  //recipe/:id/view
+  viewRecipeAPI(id:string){
+    return  this.http.get(`${this.server_url}/recipe/${id}/view`,this.appendToken())
+  }
 
+  //related-recipes?cuisine=Italian
+  getRelatedRecipeAPI(cuisine:string){
+    return  this.http.get(`${this.server_url}/related-recipes?cuisine=${cuisine}`,this.appendToken())
+  }
+
+  //recipes/673461efc82f6dc0ecec8914/download
+  downloadRecipeAPI(id:string,recipeDetails:any){
+    return  this.http.post(`${this.server_url}/recipes/${id}/download`,recipeDetails,this.appendToken())
+  }
+
+}
